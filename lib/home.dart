8ad5533d51429080b773bb5c008e4ffbc24597c6
N001-ASBL-SVC <N001-ASBL-SVC@SOCOM.MIL>
2025-04-14 09:55:34 -0400

    Make IcedContainer breathe

--- lib/home.dart
+++ lib/home.dart
@@ -45,15 +45,30 @@ class Home extends StatelessWidget {
       ),
       body: ScreenBackground(
         child: Center(
-          child: IcedContainer(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.8,
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surface,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Center(child: Text('Welcome!')),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(right: 40),
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.8,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(child: Text('Normal Container')),
+              ),
+              IcedContainer(
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.8,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(child: Text('Iced Container')),
+              ),
+            ],
           ),
         ),
       ),
--- lib/iced_container.dart
+++ lib/iced_container.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-class IcedContainer extends StatelessWidget {
+class IcedContainer extends StatefulWidget {
   final Widget child;
   final double? width;
   final double? height;
@@ -18,28 +20,63 @@ class IcedContainer extends StatelessWidget {
   });
 
   @override
+  State<IcedContainer> createState() => _IcedContainerState();
+}
+
+class _IcedContainerState extends State<IcedContainer> {
+  double blurx = 10;
+  double blury = 10;
+  double maxBlur = 50;
+  double minBlur = 10;
+  int blurDirection = 1;
+
+  double getBlur(double blur) {
+    if (blur == minBlur) {
+      setState(() {
+        blurDirection = 1;
+      });
+    } else if (blur == maxBlur) {
+      setState(() {
+        blurDirection = -1;
+      });
+    }
+    return blur + blurDirection;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(milliseconds: 100), (timer) {
+      setState(() {
+        blurx = getBlur(blurx);
+        blury = getBlur(blury);
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Positioned.fill(
-          top: 15,
+          top: 10,
           child: ImageFiltered(
-            imageFilter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+            imageFilter: ImageFilter.blur(sigmaX: blurx, sigmaY: blury),
             child: Container(
-              width: width,
-              height: height,
-              decoration: decoration?.copyWith(
+              width: widget.width,
+              height: widget.height,
+              decoration: widget.decoration?.copyWith(
                 backgroundBlendMode: BlendMode.color,
               ),
             ),
           ),
         ),
         Container(
-          width: width,
-          height: height,
-          padding: padding,
-          decoration: decoration,
-          child: child,
+          width: widget.width,
+          height: widget.height,
+          padding: widget.padding,
+          decoration: widget.decoration,
+          child: widget.child,
         ),
       ],
     );
